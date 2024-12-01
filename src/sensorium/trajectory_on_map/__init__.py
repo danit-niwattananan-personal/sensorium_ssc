9f29662ccd4f185c7268ce0bc4c918a3edbfc733
Dominik Wich <dominik.wich@tum.de>
2024-12-01 22:53:55 +0100

    Create package assignment

---
+++ src/sensorium/trajectory_on_map/__init__.py
@@ -0,0 +1,7 @@
+"""Trajectory mapping implementation."""
+
+from .dummy_trajectory import Class1
+
+__all__ = [
+    'Class1',
+]
---
+++ tests/sensorium/trajectory_on_map/test_dummy_trajectory.py
@@ -0,0 +1,9 @@
+"""Trajectory mapping test."""
+
+import pytest
+
+from src.sensorium.trajectory_on_map import Class1
+
+
+def test_class1() -> None:
+    """Test function."""
