4f4e64d113df0cb4ba204dfbb778c1eb8129dc88
Dimitar Mitev <ge63cuw@mytum.de>
2024-12-03 13:19:56 +0100

    trajectory test

---
+++ src/sensorium/trajectory/trajectory_code.py
@@ -0,0 +1,13 @@
+# Copyright 2024  Projektpraktikum Python.
+# SPDX-License-Identifier: Apache-2.0
+"""Dummy_package."""
+
+from __future__ import annotations
+
+
+def say_hello():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    say_hello()
