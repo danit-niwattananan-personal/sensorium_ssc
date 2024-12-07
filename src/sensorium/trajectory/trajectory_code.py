0e0470241de026dc28ea70e6cfec0b7afff134aa
Dimitar Mitev <ge63cuw@mytum.de>
2024-12-07 14:18:06 +0100

    Implement bubblesort

--- src/sensorium/trajectory/trajectory_code.py
+++ src/sensorium/trajectory/trajectory_code.py
@@ -2,12 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 """Dummy_package."""
 
-from __future__ import annotations
 
-
-def say_hello() -> str:
-    return "Hello, World!"
-
-
-if __name__ == "__main__":
-    say_hello()
+def hello() -> str:
+    """Says Hello."""
+    return 'Hello!'
