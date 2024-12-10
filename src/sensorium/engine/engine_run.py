919494e066cc65307081a26e5f2b71cf2c3379d6
Raymund Tonyka <ge45xub@mytum.de>
2024-12-10 16:48:29 +0100

    first layout of the client engine

--- src/sensorium/engine/engine_run.py
+++ src/sensorium/engine/engine_run.py
@@ -1,7 +1,7 @@
 # Copyright 2024  Projektpraktikum Python.
 # SPDX-License-Identifier: Apache-2.0
 
-"""Dummy_package."""
+"""GUI."""
 
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QAction
@@ -44,7 +44,7 @@ class MainWindow(QMainWindow):
 
         # Platzhalter-Label für Videoplayer
         placeholder = QLabel('Hier kommt der Videoplayer')
-        placeholder.setAlignment(Qt.AlignCenter) # type: ignore[attr-defined]
+        placeholder.setAlignment(Qt.AlignCenter)  # type: ignore[attr-defined]
         right_layout.addWidget(placeholder, stretch=1)
 
         # Menüleiste
@@ -54,20 +54,27 @@ class MainWindow(QMainWindow):
         # Menü: Einstellungen  # noqa: ERA001
         settings_menu = menu_bar.addMenu('Einstellungen')
         settings_action = QAction('Einstellungen öffnen', self)
+        settings_action.setObjectName('action_settings')
         settings_action.triggered.connect(self.open_settings)
         settings_menu.addAction(settings_action)
 
         # Menü: Server verbinden
         server_menu = menu_bar.addMenu('Server')
-        server_action = QAction('Mit Server verbinden', self)
-        server_action.triggered.connect(self.connect_server)
-        server_menu.addAction(server_action)
+        server_connect = QAction('Mit Server verbinden', self)
+        server_connect.triggered.connect(self.connect_server)
+        server_disconnect = QAction('Verbindung trennen', self)
+        server_disconnect.triggered.connect(self.disconnect_server)
+        server_menu.addAction(server_connect)
+        server_menu.addAction(server_disconnect)
 
         # Menü: Dateien vom Server anfragen
-        settings_menu = menu_bar.addMenu('Dateien')
-        settings_action = QAction('Dateien vom Server anfragen', self)
-        settings_action.triggered.connect(self.ask_for_data)
-        settings_menu.addAction(settings_action)
+        ask_data_menu = menu_bar.addMenu('Dateien')
+        ask_data_action = QAction('Dateien vom Server anfragen', self)
+        ask_4_new_data = QAction('Dateien neu anfragen', self)
+        ask_data_action.triggered.connect(self.ask_for_data)
+        ask_4_new_data.triggered.connect(self.ask_for_data)
+        ask_data_menu.addAction(ask_data_action)
+        ask_data_menu.addAction(ask_4_new_data)
 
     def open_settings(self) -> None:
         """Hier wird später das fenster für die Einstellungen geöffnet."""
@@ -76,6 +83,10 @@ class MainWindow(QMainWindow):
     def connect_server(self) -> None:
         """Hier wird später das Pop-Up für den Aufbau der Verbindung geöffnet."""
         print('Mit Server verbunden')
+
+    def disconnect_server(self) -> None:
+        """Hier wird später die Verbindung zum Server getrennt."""
+        print('Verbindung zum Server getrennt')
 
     def ask_for_data(self) -> None:
         """Hier werden später die Ordner vom Server geladen."""
