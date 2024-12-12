5945287d198641c55f3d6ed019065051a13132d6
Antonija <ge63rez@mytum.de>
2024-12-12 10:14:10 +0100

    Folder moved to sensorium

--- tests/sensorium/test_data_processing/test_module_antonija.py
+++ tests/sensorium/test_data_processing/test_module_antonija.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 """Test Hello."""
 
-from data_processing.dummy_module_antonija import hello
+from sensorium.data_processing.dummy_module_antonija import hello
 
 
 def test_hello() -> None:
