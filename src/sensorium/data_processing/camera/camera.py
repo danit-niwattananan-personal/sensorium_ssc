d05b698d53211d42bd196f97ab27753bcb8f4074
Antonija <ge63rez@mytum.de>
2024-12-18 12:56:39 +0100

    added dummy data

--- src/sensorium/data_processing/camera/camera.py
+++ src/sensorium/data_processing/camera/camera.py
@@ -7,7 +7,7 @@ import cv2
 """load images"""
 
 im = cv2.imread(
-    '/Users/antonijakrajcheva/Desktop/kitti_dummy_camera/kitti_sequence_00/images/1.png',
+    'dummy_data/kitti_sequence_00/images/1.png',
     cv2.IMREAD_COLOR,
 )
 cv2.imshow('Image1', im)
