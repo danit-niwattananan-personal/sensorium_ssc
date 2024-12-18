20d5a0d55c67c7aeb886bbfef55e1bb53693bf90
Antonija <ge63rez@mytum.de>
2024-12-18 12:35:31 +0100

    deleted code to try to pass pipeline

--- src/sensorium/data_processing/camera/camera.py
+++ src/sensorium/data_processing/camera/camera.py
@@ -1,14 +1,3 @@
 # Copyright 2024  Projektpraktikum Python.
 # SPDX-License-Identifier: Apache-2.0
 """Camera."""
-
-import cv2
-
-"""load images"""
-
-im = cv2.imread(
-    'kitti_dummy/kitti_sequence_00/images/1.png',
-    cv2.IMREAD_COLOR,
-)
-cv2.imshow('Image1', im)
-cv2.waitKey(5000)
