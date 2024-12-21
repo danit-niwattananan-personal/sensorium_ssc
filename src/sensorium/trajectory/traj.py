31dfa0622c0a3579510999d4e5f337015784cb21
Dimitar Mitev <ge63cuw@mytum.de>
2024-12-21 17:57:12 +0100

    processing trajectory data

--- src/sensorium/trajectory/traj.py
+++ src/sensorium/trajectory/traj.py
@@ -2,10 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 """Trajectory Server Functions."""
 
-from pathlib import Path
 import numpy as np
 from numpy.typing import NDArray
-
+from pathlib import Path
 
 def parse_calibration(filename: str) -> dict[str, NDArray[np.float64]]:
     """
@@ -18,7 +17,7 @@ def parse_calibration(filename: str) -> dict[str, NDArray[np.float64]]:
         dict: Calibration matrices as 4x4 numpy arrays.
     """
     calib = {}
-    with Path(filename).open() as calib_file:  # Use Path.open() instead of open()
+    with Path(filename).open() as calib_file:
         for line in calib_file:
             key, content = line.strip().split(':')
             calib[key] = np.array([float(x) for x in content.split()])
@@ -39,7 +38,7 @@ def parse_poses(filename: str, calibration: dict[str, NDArray[np.float64]]) -> l
     tr = calibration['Tr']
     tr_inv = np.linalg.inv(tr)
     
-    with Path(filename).open() as file:  # Use Path.open() instead of open()
+    with Path(filename).open() as file:
         for line in file:
             values = [float(v) for v in line.strip().split()]
             pose = np.array(values).reshape(4, 4)
@@ -70,7 +69,6 @@ def save_trajectory(trajectory: list[dict[str, float]]) -> None:
     Args:
         trajectory (list): The trajectory data to save.
     """
-    pass
 
 
 
