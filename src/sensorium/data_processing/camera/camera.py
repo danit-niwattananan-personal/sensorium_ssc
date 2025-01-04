0f78f5c58bae15d91b426df5ed31eacedf0d25ae
Antonija <ge63rez@mytum.de>
2025-01-04 19:25:16 +0100

    tried to write test for camera but failed. Can't fix mypy problem in camera function

--- src/sensorium/data_processing/camera/camera.py
+++ src/sensorium/data_processing/camera/camera.py
@@ -7,27 +7,15 @@ from pathlib import Path
 
 import cv2
 
+dir1 = '/Users/antonijakrajcheva/b/src/sensorium/data_processing/camera/dummy_kitti/images'
 
-def load_images(dir):  # noqa: A002, ANN001, ANN201
+
+def load_images(directory: str) -> list:
     """Read images.
 
     Dir is the path to the image directory.
     The function returns a list with all frames.
     """
-    dir = '/Users/antonijakrajcheva/b/src/sensorium/data_processing/camera/dummy_kitti/images'  # noqa: A001
-    image_paths = [Path(dir) / file for file in sorted(os.listdir(dir))]
+    image_paths = [Path(directory) / file for file in sorted(os.listdir(directory))]
 
     return [cv2.imread(str(path), cv2.IMREAD_UNCHANGED) for path in image_paths]
-
-
-# for testing that the function works:
-def iterate(list):
-    yield from list
-
-
-dir = '/Users/antonijakrajcheva/b/src/sensorium/data_processing/camera/dummy_kitti/images'  # noqa: A001
-frame_length = 300
-
-for val in iterate(load_images(dir)):
-    cv2.imshow('Camera', val)
-    cv2.waitKey(frame_length)
--- tests/sensorium/test_data_processing/test_camera/test_camera.py
+++ tests/sensorium/test_data_processing/test_camera/test_camera.py
@@ -1,10 +1,28 @@
 # SPDX-License-Identifier: Apache-2.0
 """Test Camera."""
 
-import pytest
+import os
 
-from sensorium.data_processing.camera.camera_simple import camera_function
+import cv2
+
+from sensorium.data_processing.camera.camera import load_images
+
+# # for testing that the function works:
+# def iterate(list1) -> list:
+#     """Simple function for iteration through list."""
+#     yield from list1
+
+# for val in iterate(load_images(dir1)):
+#     frame_length = 300
+#     cv2.imshow('Camera', val)
+#     cv2.waitKey(frame_length)
 
 
 def test_camera_function() -> None:
-    """Test for simple camera."""
+    """Test for camera."""
+    dir1 = '/Users/antonijakrajcheva/b/src/sensorium/data_processing/camera/dummy_kitti/images'
+    for file in sorted(os.listdir(dir1)):
+        assert image_paths == [Path(dir1) / file]
+        assert load_images(dir1) == [
+            cv2.imread(str(path), cv2.IMREAD_UNCHANGED) for path in [Path(dir1) / file]
+        ]
