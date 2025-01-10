bee5d6d0241a66707e4a455ecf16064e64d380de
Dominik Wich <dominik.wich@tum.de>
2025-01-10 21:56:47 +0100

    Removed old files (bin_to_ply.py and test_bin_to_ply.py) after renaming

--- src/sensorium/lidar_pointcloud/bin_to_ply.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2024  Projektpraktikum Python.
-# SPDX-License-Identifier: Apache-2.0
-"""dummy module."""
-
-
-def bin_to_ply_function() -> int:
-    """Dummy function."""
-    return 1
