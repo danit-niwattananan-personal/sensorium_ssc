98a95bb02204e7821c277bc7fad6b1d16e054665
Dominik Wich <dominik.wich@tum.de>
2025-01-10 23:19:14 +0100

    update test_lidar_visualization

--- tests/sensorium/lidar_pointcloud/test_lidar_visualization.py
+++ tests/sensorium/lidar_pointcloud/test_lidar_visualization.py
@@ -1,20 +1,3 @@
-# Copyright 2024  Projektpraktikum Python.
-# SPDX-License-Identifier: Apache-2.0
+# # Copyright 2024  Projektpraktikum Python.
+# # SPDX-License-Identifier: Apache-2.0
 """dummy module."""
-
-import pytest
-
-from sensorium.lidar_pointcloud.lidar_visualization import PointcloudVis
-
-
-@pytest.fixture
-def pointcloud() -> PointcloudVis:
-    """."""
-    return PointcloudVis()
-
-
-def test_jump_forwards(pointcloud: PointcloudVis) -> None:
-    """."""
-    initial_frame = pointcloud.frame_number
-    pointcloud.jump_forwards()
-    assert pointcloud.frame_number == initial_frame + 10
