4f303e2d24bb6d829e0cb61e0b817acced90acde
Dominik Wich <dominik.wich@tum.de>
2025-01-13 19:33:00 +0100

    update test camera

--- tests/sensorium/test_camera_visualization/test_camera_visualization.py
+++ tests/sensorium/test_camera_visualization/test_camera_visualization.py
@@ -1,35 +1,9 @@
 # Copyright 2024  Projektpraktikum Python.
 # SPDX-License-Identifier: Apache-2.0
-# """Camera Visualization."""
+"""Test camera visualization."""
 
-# from unittest.mock import Mock, patch
-
-# from PySide6.QtCore import Qt
-# from PySide6.QtGui import QPixmap
-
-# from sensorium.camera_visualization.camera_visualization import CameraWidget
-
-
-# def test_update_image() -> None:
-#     """."""
-#     camera_widget = CameraWidget()
-#     with patch('PySide6.QtGui.QPixmap') as mock_pixmap:
-#         pixmap = Mock()
-#         pixmap.scaled.return_value = pixmap
-#         mock_pixmap.return_value = pixmap
-#         camera_widget.update_image()
-#         pixmap.scaled.assert_called_once_with(
-#             camera_widget.label.width(),
-#             camera_widget.label.height(),
-#             Qt.AspectRatioMode.KeepAspectRatio,
-#         )
-#     assert camera_widget.label.setPixmap.call_count == 1
-
-
-import sys
 from unittest.mock import Mock, patch
 
-import pytest
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QApplication
@@ -39,31 +13,25 @@ from sensorium.camera_visualization.camera_visualization import (
 )
 
 
-# @pytest.fixture
-# def start_application() -> QApplication:
-#     """Fixture zum Erstellen und Initialisieren des MainWindow."""
-#     return QApplication([])
-
-
 def test_update_image() -> None:
     """."""
     app = QApplication([])
-    # start_application
-    camera_widget = CameraWidget()
+    try:
+        camera_widget = CameraWidget()
+        camera_widget.frame_id = 99
+        with patch('sensorium.camera_visualization.camera_visualization.QPixmap') as mock_pixmap:
+            mock_pixmap_instance = Mock(spec=QPixmap)
+            mock_pixmap_instance.scaled.return_value = QPixmap()
+            mock_pixmap.return_value = mock_pixmap_instance
 
-    with patch('sensorium.camera_visualization.camera_visualization.QPixmap') as mock_pixmap:
-        mock_pixmap_instance = Mock()
-        mock_pixmap.return_value = mock_pixmap_instance
-        mock_pixmap_instance.scaled.return_value = mock_pixmap_instance
-
-        camera_widget.update_image()
-
-        mock_pixmap.assert_called_once_with(
-            camera_widget.img_directory + '/' + f'{camera_widget.frame_number - 1:010d}.png'
-        )
-        mock_pixmap_instance.scaled.assert_called_once_with(
-            camera_widget.label.width(),
-            camera_widget.label.height(),
-            Qt.AspectRatioMode.KeepAspectRatio,
-        )
-    app.shutdown()
+            camera_widget.update_image()
+            assert camera_widget.frame_id == 0
+            mock_pixmap_instance.scaled.assert_called_once_with(
+                camera_widget.label.width(),
+                camera_widget.label.height(),
+                Qt.AspectRatioMode.KeepAspectRatio,
+            )
+            camera_widget.update_image()
+            assert camera_widget.frame_id == 1
+    finally:
+        app.shutdown()
