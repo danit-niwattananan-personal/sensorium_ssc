96e8fc0e111b74d28303920117cb671fad3cc7b3
Dominik Wich <dominik.wich@tum.de>
2025-01-14 23:02:12 +0100

    fixed pytest error

--- tests/sensorium/test_camera_visualization/test_camera_visualization.py
+++ tests/sensorium/test_camera_visualization/test_camera_visualization.py
@@ -4,6 +4,7 @@
 
 from unittest.mock import Mock, patch
 
+import numpy as np
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QApplication
@@ -16,13 +17,11 @@ from sensorium.camera_visualization.camera_visualization import (
 def test_show_image() -> None:
     """."""
     app = QApplication([])
+    mock_img = np.zeros((375, 1242, 3), dtype=np.uint8)
     try:
-        camera_widget = CameraWidget()
-        camera_widget.frame_id = 99
-        with patch('sensorium.camera_visualization.camera_visualization.cv2.imread') as mock_imread:
-            mock_img = Mock()
-            mock_img.shape = (480, 640, 3)
-            mock_imread.return_value = mock_img
+        with patch('cv2.imread', return_value=mock_img):
+            camera_widget = CameraWidget()
+            camera_widget.frame_id = 99
             with patch(
                 'sensorium.camera_visualization.camera_visualization.QPixmap'
             ) as mock_pixmap:
