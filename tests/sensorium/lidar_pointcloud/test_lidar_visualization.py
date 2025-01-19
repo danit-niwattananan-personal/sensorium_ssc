7881f1547bee4f5371d7a77bc5b521ea849a7271
Dominik Wich <dominik.wich@tum.de>
2025-01-19 13:23:08 +0100

    added qtbot and skipif fixture to lidar_visualization test

--- tests/sensorium/lidar_pointcloud/test_lidar_visualization.py
+++ tests/sensorium/lidar_pointcloud/test_lidar_visualization.py
@@ -2,46 +2,39 @@
 # # SPDX-License-Identifier: Apache-2.0
 """dummy module."""
 
+import os
 from unittest.mock import patch
 
 import numpy as np
 import pygfx as gfx  # type: ignore[import-untyped]
 import pytest
-from PySide6 import QtWidgets
+from pytestqt.qtbot import QtBot  # type: ignore[import-untyped]
 from wgpu.gui.qt import WgpuCanvas  # type: ignore[import-untyped]
 
 from sensorium.lidar_pointcloud.lidar_visualization import PointcloudVis
 
 
 @pytest.fixture
-def app() -> QtWidgets.QApplication:
-    """."""
-    return QtWidgets.QApplication([])
-
-
-@pytest.fixture
 def pointcloud_vis() -> PointcloudVis:
     """Fixture to create a PointcloudVis instance."""
-    with patch('wgpu.gui.qt.get_alt_x11_display', return_value=1):
-        return PointcloudVis()
+    return PointcloudVis()
 
 
-def test_setup_canvas_initialization(
-    app: QtWidgets.QApplication, pointcloud_vis: PointcloudVis
-) -> None:
+@pytest.mark.skipif(bool(os.getenv('CI')), reason='no windowing system available in CI')
+def test_setup_canvas_initialization(qtbot: QtBot, pointcloud_vis: PointcloudVis) -> None:
     """Test setup_canvas method."""
+    qtbot.addWidget(pointcloud_vis)
     pointcloud_vis.setup_canvas()
     assert isinstance(pointcloud_vis.canvas, WgpuCanvas)
     assert isinstance(pointcloud_vis.renderer, gfx.WgpuRenderer)
     assert isinstance(pointcloud_vis.scene, gfx.Scene)
     assert isinstance(pointcloud_vis.camera, gfx.OrthographicCamera)
 
-    app.shutdown()
-    app.quit()
 
-
-def test_update_scene(app: QtWidgets.QApplication, pointcloud_vis: PointcloudVis) -> None:
+@pytest.mark.skipif(bool(os.getenv('CI')), reason='no windowing system available in CI')
+def test_update_scene(qtbot: QtBot, pointcloud_vis: PointcloudVis) -> None:
     """Test update_scene method."""
+    qtbot.addWidget(pointcloud_vis)
     pointcloud_vis.setup_canvas()
     frame_id = 0
     mock_positions = np.array([[1.0, 1.0, 1.0], [2.0, 2.0, 2.0]], dtype=np.float32)
@@ -53,6 +46,3 @@ def test_update_scene(app: QtWidgets.QApplication, pointcloud_vis: PointcloudVis
         mock_load_positions.assert_called_once_with(frame_id)
         frame_id += 1
         pointcloud_vis.update_scene(frame_id)
-
-    app.shutdown()
-    app.quit()
