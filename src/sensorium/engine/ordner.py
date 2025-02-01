ce5ebac164c20cf67d122a16646f2c6dc4c4e285
Raymund Tonyka <ge45xub@mytum.de>
2025-02-01 10:47:04 +0100

    added tests

--- src/sensorium/engine/ordner.py
+++ src/sensorium/engine/ordner.py
@@ -26,8 +26,8 @@ class ButtonPanel(QWidget):
         layout.addWidget(self.info_label)
 
         for i in range(16):
-            button = QPushButton(f'Sequenz {i}', self)
-            button.setStyleSheet("""
+            self.button = QPushButton(f'Sequenz {i}', self)
+            self.button.setStyleSheet("""
                 QPushButton {
                     background-color: lightgray;
                     padding: 10px;
@@ -45,8 +45,8 @@ class ButtonPanel(QWidget):
                     border: 1px solid gray;
                 }
             """)
-            button.clicked.connect(self.update_seq)
-            layout.addWidget(button)
+            self.button.clicked.connect(self.update_seq)
+            layout.addWidget(self.button)
 
         self.setLayout(layout)
 
