491d8f6c9e0a36bac83fb9daaebb585ca14d8e52
Raymund Tonyka <ge45xub@mytum.de>
2025-02-02 14:32:58 +0100

    more gui test for engine_run and visualizationgui

--- tests/sensorium/engine/test_ordner.py
+++ tests/sensorium/engine/test_ordner.py
@@ -15,12 +15,30 @@ from sensorium.engine.visualization_gui import VisualisationGui
 
 
 @pytest.mark.skipif(bool(os.getenv('CI')), reason='no windowing system available in CI')
-def test_update_scene(qtbot: QtBot) -> None:
-    """Testet update_scene."""
+def test_ordner(qtbot: QtBot) -> None:
+    """Testet ordner."""
     vizualisation = VisualisationGui()
     ordner = ButtonPanel(vizualisation)
-
     qtbot.addWidget(ordner)
+
     with patch('sensorium.engine.ordner.ButtonPanel.update_seq') as mock_update_seq:
         qtbot.mouseClick(ordner.button, Qt.LeftButton)  # type: ignore[attr-defined]
         mock_update_seq.assert_called_once()
+
+
+@pytest.mark.skipif(bool(os.getenv('CI')), reason='no windowing system available in CI')
+def test_update_seq(qtbot: QtBot) -> None:
+    """Testet ordner."""
+    vizualisation = VisualisationGui()
+    ordner = ButtonPanel(vizualisation)
+    qtbot.add_widget(ordner)
+
+    with patch(
+        'sensorium.engine.visualization_gui.VisualisationGui.update_frame'
+    ) as mock_update_frame:
+        vizualisation.seq_id = 4
+        vizualisation.framenumber = 10
+        qtbot.mouseClick(ordner.button, Qt.LeftButton)  # type: ignore[attr-defined]
+        assert vizualisation.seq_id == 9
+        assert vizualisation.framenumber == 0
+        mock_update_frame.assert_called_once_with(0)
